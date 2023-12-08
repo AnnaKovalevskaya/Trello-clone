804f93cd6b9b17b70afaf7750ddfdeefef19b4d4
Anna <annakovalevskaya19@gmail.com>
2023-12-08 15:52:34 +0300

    init(localStorage.js): add localStorage support

---
+++ src/js/localStorage.js
@@ -0,0 +1,13 @@
+let todos = getData();
+
+// получение данных
+function setData() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+// сохранение данных
+function getData() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
+
+export { setData, todos };
