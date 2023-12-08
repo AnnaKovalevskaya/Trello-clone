55431dac990edb6ad41fae7788e3a4395524947f
Anna <annakovalevskaya19@gmail.com>
2023-12-08 14:30:33 +0300

    init(index.html): add modal

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -7,9 +7,16 @@ function $(selector) {
 function $$(selector) {
   return document.querySelectorAll(selector)
 }
-
+const sectionTodoEl = $('.todo');
+const sectionProgressEl = $('.in-progress');
+const sectionDoneEl = $('.done');
 const timeEl = $('time');
+const btnDeleteAllDoneEl = $('#btnDeleteAllCards');
 
 export {
   timeEl,
+  sectionTodoEl,
+  sectionProgressEl,
+  sectionDoneEl,
+  btnDeleteAllDoneEl,
 }
